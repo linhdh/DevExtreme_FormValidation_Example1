c567bef05ef9152b02dcac8cbf55a8d426e9f282
Dao Hai Linh <linhdh@live.com>
2022-12-07 15:40:42 +0700

    project for DevExtreme support

--- TextboxValidationExample1/src/app/pages/home/home.component.ts
+++ TextboxValidationExample1/src/app/pages/home/home.component.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
 })
 
 export class HomeComponent {
-  versionPattern: RegExp = /^\d{1,4}\.\d{1,4}$|^\d{1,4}\.\d{1,4}\.\d{1,4}$|^\d{1,4}\.\d{1,4}\.\d{1,4}\.\d{1,4}$/gm;
+  //versionMessage11: string = "Version must have 'x.x' pattern. x is positive integer number in range 0 - 9999.";
+  //versionMessage12: string = "Version must have 'x.x.x' pattern. x is positive integer number in range 0 - 9999.";
+  //versionMessage13: string = "Version must have 'x.x.x.x' pattern. x is positive integer number in range 0 - 9999.";
+  //versionPattern: RegExp = /^\d{1,4}\.\d{1,4}$|^\d{1,4}\.\d{1,4}\.\d{1,4}$|^\d{1,4}\.\d{1,4}\.\d{1,4}\.\d{1,4}$/gm;
+  //versionPattern11: RegExp = /^\d{1,4}\.\d{1,4}$/gm;
+  //versionPattern12: RegExp = /^\d{1,4}\.\d{1,4}\.\d{1,4}$/gm;
+  //versionPattern13: RegExp = /^\d{1,4}\.\d{1,4}\.\d{1,4}\.\d{1,4}$/gm;
+  //versionPattern2: RegExp = /^[0-9]{1,4}\.[0-9]{1,4}(\.(?=[0-9]{1,4})(?<=\.)[0-9]{1,4})*$/gm;
+  //versionPattern3: RegExp = /^[0-9]{1,4}\.[0-9]{1,4}(\.(?=[0-9]{1,4})(?<=\.)[0-9]{1,4}){0,2}$/gm;
+  //versionPattern4: RegExp = /^[0-9]{1,4}(\.[0-9]{1,4}){1,3}$/gm;
+  versionPattern5: RegExp = /^[0-9]{1,4}\.[0-9]{1,4}(\.(?=[0-9]{1,4})(?<=\.)[0-9]{1,4}){0,2}$/gm;
+  versionMessage: string = "Version must have 'x.x', 'x.x.x' or 'x.x.x.x' pattern. x is positive integer number in range 0 - 9999.";
    
   constructor() {}
+
+  handleValidated(event: any): void {
+    //console.log(event);
+  }
+
+  handleOptionChanged(event: any): void {
+    console.log(event);
+  }
 }
